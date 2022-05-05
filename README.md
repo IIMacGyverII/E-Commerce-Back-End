44cedbb3c6660bc95f68cc23a537c7ab5861e945
Joshua Christianson <macgyverjunk@yahoo.com>
2022-05-05 17:23:04 -0500

    seeds and index should be done and working

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# E-Commerce-Back-End
+
+# Project Title: Object-Relational Mapping (ORM): E-Commerce Back End
+## Description:
+    Comming Soon
+## What motivated me:
+    Comming Soon
+## Why did I build this project?
+    Comming Soon
+## What did you learn?
+    Comming Soon
+## Installation
+    1. Comming Soon 
+## Usage
+    1. Comming Soon.  2. Comming Soon
+## Demonstration Video(Click to play)
+[![Demo Video](https://img.youtube.com/vi/fh5s7yljpbw/0.jpg)](https://www.youtube.com/watch?v=fh5s7yljpbw)
+## Credit
+    All my instructors at Northwestern and my study groups.
+## Licenses Used
+No licenses used
--- models/Product.js
+++ models/Product.js
@@ -2,7 +2,6 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
-
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
@@ -10,7 +9,39 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+    },
   },
+    stock: {
+      type: DataTypes.INTEGER,
+      defaultValue: 10,
+      allowNull: false,
+      validate: {
+        isNumeric: true
+    },
+  },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+    },
+  },
+},
   {
     sequelize,
     timestamps: false,
